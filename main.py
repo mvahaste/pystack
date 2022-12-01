9e3173ef63994974d1b53dd7b59953da55e57601
M <48407846+mvahaste@users.noreply.github.com>
2022-12-01 18:04:19 +0200

    Added sfx, ability to retry

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class Box:
         if left < 0 or right > SCREEN_X:
             self.speed *= -1
 
+            bounce_sound.play()
+
         self.x += self.speed
 
 
@@ -84,9 +86,15 @@ screen = pygame.display.set_mode(RESOLUTION)
 pygame.display.set_caption("pyStack")
 clock = pygame.time.Clock()  # Sync FPS
 
+place_sound = pygame.mixer.Sound("place.wav")
+loss_sound = pygame.mixer.Sound("loss.wav")
+bounce_sound = pygame.mixer.Sound("bounce.wav")
+
 color_manager = Colors()
 
 score = 0
+
+status = "playing"
 
 # Tower
 tower = []
@@ -103,37 +111,59 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == 32:
-                last_box = tower[-1]
+                if status == "playing":
+                    last_box = tower[-1]
 
-                current_left = player.x
-                current_right = player.x + player.w
+                    current_left = player.x
+                    current_right = player.x + player.w
 
-                last_left = last_box.x
-                last_right = last_box.x + last_box.w
+                    last_left = last_box.x
+                    last_right = last_box.x + last_box.w
 
-                # * Overlap left
-                if current_left < last_left:
-                    player.x = last_box.x
-                    player.w -= last_right - current_right
+                    # * Overlap left
+                    if current_left < last_left:
+                        player.x = last_box.x
+                        player.w -= last_right - current_right
+                    # * Overlap right
+                    elif current_right > last_right:
+                        player.w -= current_right - last_right
 
-                # * Overlap right
-                elif current_right > last_right:
-                    player.w -= current_right - last_right
+                    if player.w <= 0:
+                        print("You lose!")
+                        status = "lost"
+                        loss_sound.play()
+                    else:
+                        place_sound.play()
 
-                if player.w <= 0:
-                    print("You lose!")
-                    pygame.quit()
+                    tower.append(Box(player.x, player.y, player.w, player.h, 0, player.color))
 
-                tower.append(Box(player.x, player.y, player.w, player.h, 0, player.color))
+                    for box in tower:
+                        box.y += 30
 
-                for box in tower:
-                    box.y += 30
+                    # Move the player to the left or right side of the screen
+                    player.x = choice([0, SCREEN_X - player.w])
 
-                player.x = choice([0, SCREEN_X - player.w])
-                player.color = color_manager.gradient_color()
+                    # Set the speed to the correct direction so the box doesn't bounce the instant it spawns
+                    if player.x == 0:
+                        player.speed = 4
+                    elif player.x == SCREEN_X - player.w:
+                        player.speed = -4
 
-                score += 1
-                print(f"Score: {score}; Speed: {player.speed}")
+                    player.color = color_manager.gradient_color()
+
+                    score += 1
+                    print(f"Score: {score}; Speed: {player.speed}")
+                elif status == "lost":
+                    # Reset the game
+                    status = "playing"
+                    tower = []
+
+                    for i in range(1, 11):
+                        tower.append(Box((SCREEN_X / 2) - 120, SCREEN_Y - (30 * i), 240, 30, 1, color_manager.gradient_color()))
+
+                    player = create_box(0, 0, 240, 30, 4, color_manager.gradient_color())
+                    player.y = tower[-1].y - 30
+                    score = 0
 
         if event.type == pygame.QUIT:
             pygame.quit()
