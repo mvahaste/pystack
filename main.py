adf6437f8aa042ff209f675c5cb464720f8c21aa
M <48407846+mvahaste@users.noreply.github.com>
2022-12-02 20:21:56 +0200

    *

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pygame
 from random import choice
 
 
-def create_box(x: int, y: int, w: int, h: int, speed: int):
-    return Box(x, SCREEN_Y - 50 - y, w, h, speed)
-
-
 class Box:
     def __init__(self, x: int, y: int, w: int, h: int, speed: int) -> None:
         self.x = x
@@ -15,9 +11,12 @@ class Box:
         self.speed = speed
 
     def draw(self) -> None:
-        pygame.draw.rect(screen, (170, 74, 68), pygame.Rect(self.x, self.y, self.w, self.h))
+        """Draws the box on the screen"""
+        self.image = pygame.transform.scale(brick_img, (self.w, self.h))
+        screen.blit(self.image, (self.x, self.y))
 
     def move(self) -> None:
+        """Moves the box"""
         left = self.x
         right = self.x + self.w
 
@@ -30,14 +29,17 @@ class Box:
 
 
 class Button:
-    def __init__(self, x, y, image, hover_image) -> None:
+    def __init__(self, x: int, y: int, image: pygame.Surface, hover_image: pygame.Surface) -> None:
+        # Scale the images
         self.image = pygame.transform.scale(image, (int(image.get_width() * 0.6), int(image.get_height() * 0.6)))
         self.hover = pygame.transform.scale(hover_image, (int(hover_image.get_width() * 0.6), int(hover_image.get_height() * 0.6)))
+        # Position the images
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.pressed = False
 
-    def draw(self):
+    def draw(self) -> bool:
+        """Draws the button, returns True if it has been pressed"""
         mouse_pos = pygame.mouse.get_pos()
         image = self.image
 
@@ -53,19 +55,16 @@ class Button:
         return self.pressed
 
 
+def create_box(x: int, y: int, w: int, h: int, speed: int) -> Box:
+    """Generates a box object"""
+    return Box(x, SCREEN_Y - 50 - y, w, h, speed)
+
+
+# Set constant variables
 SCREEN_X = 480
 SCREEN_Y = 640
 RESOLUTION = (SCREEN_X, SCREEN_Y)
-FPS = 60
-
-# Define Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# TODO: visuals, sfx
+FPS = 240
 
 # Initialize pygame and create window
 pygame.init()
@@ -74,7 +73,7 @@ screen = pygame.display.set_mode(RESOLUTION)
 pygame.display.set_caption("pyStack")
 clock = pygame.time.Clock()  # Sync FPS
 
-# Sound effects
+# Load sound effects
 place_sound = pygame.mixer.Sound("place.wav")
 place_sound.set_volume(0.25)
 loss_sound = pygame.mixer.Sound("loss.wav")
@@ -82,9 +81,8 @@ loss_sound.set_volume(0.25)
 bounce_sound = pygame.mixer.Sound("bounce.wav")
 bounce_sound.set_volume(0.25)
 
-score = 0
-
-status = "playing"
+# Load images
+brick_img = pygame.image.load("brick.png").convert()
 
 retry_img = pygame.image.load("retry.png").convert_alpha()
 retry_hover = pygame.image.load("retry_hover.png").convert_alpha()
@@ -93,27 +91,33 @@ exit_img = pygame.image.load("quit.png").convert_alpha()
 exit_hover = pygame.image.load("quit_hover.png").convert_alpha()
 
 bg_img = pygame.image.load("background.jpg")
+# Offset for the background image
 bg_y = -640 * 2
 
+# Create buttons
 retry_button = Button(50, 550, retry_img, retry_hover)
 exit_button = Button(250, 550, exit_img, exit_hover)
 
-# Tower
+# Tower setup
 tower = []
-
-score_font = pygame.font.Font("LCDMono2.ttf", 60)
-
 
 for i in range(1, 11):
     tower.append(Box((SCREEN_X / 2) - 120, SCREEN_Y - (30 * i), 240, 30, 1))
 
 
-# Player
-player = create_box(0, 0, 240, 30, 4)
+# Score setup
+score_font = pygame.font.Font("LCDMono2.ttf", 60)
+score = 0
+status = "playing"
+
+# Player setup
+player = create_box(0, 0, 240, 30, 1)
 player.y = tower[-1].y - 30
 
+# God mode easter egg / debug, activate by pressing 'G'
 god_mode = False
 
+# Game loop
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -125,18 +129,19 @@ while True:
                 if status == "playing":
                     last_box = tower[-1]
 
+                    # Variables for easier reading
                     current_left = player.x
                     current_right = player.x + player.w
 
                     last_left = last_box.x
                     last_right = last_box.x + last_box.w
 
-                    # * Overlap left
+                    # Overlap left
                     if not god_mode:
                         if current_left < last_left:
                             player.x = last_box.x
                             player.w -= last_right - current_right
-                        # * Overlap right
+                        # Overlap right
                         elif current_right > last_right:
                             player.w -= current_right - last_right
 
@@ -148,6 +153,7 @@ while True:
 
                         tower.append(Box(player.x, player.y, player.w, player.h, 0))
 
+                        # Move all boxes down to create the illusion of a stack
                         for box in tower:
                             box.y += 30
                         else:
@@ -158,19 +164,22 @@ while True:
 
                         # Set the speed to the correct direction so the box doesn't bounce the instant it spawns
                         if player.x == 0:
-                            player.speed = 4 + (score / 5)
+                            player.speed = 1 + (score / 5)
                         elif player.x == SCREEN_X - player.w:
-                            player.speed = -4 - (score / 5)
+                            player.speed = -1 - (score / 5)
 
                         score += 1
 
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    screen.fill(BLACK)
-
-    # Draw background image
+    # Draw the background
+    screen.fill((0, 0, 0))
     screen.blit(bg_img, (0, bg_y))
+
+    # Draw the score
+    text_surface = score_font.render(str(score), True, (255, 255, 255))
+    screen.blit(text_surface, (SCREEN_X / 2 - text_surface.get_width() / 2, 50))
 
     if status == "playing":
         player.draw()
@@ -178,10 +187,6 @@ while True:
 
     for box in tower:
         box.draw()
-
-    text_surface = score_font.render(str(score), True, (255, 255, 255))
-
-    screen.blit(text_surface, (SCREEN_X / 2 - text_surface.get_width() / 2, 50))
 
     if status == "lost":
         if retry_button.draw():
